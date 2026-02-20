c2a2fdcbf9dc082dacd71dfd1d8da0a0df469ff5
Mariana Suarez <mariana.suarezh97@gmail.com>
2026-02-20 00:40:23 -0500

    Personalizacion del componente app con mi nombre

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App()
+{ const fecha = new Date().toLocaleString();
+ return ( <main><h1>Hola, Soy Mariana aprendiz-ADSO</h1>
+ <p>Que espero aprender de este curso "Ract?"
+  En este curso de React me gustaría aprender a crear aplicaciones paso a paso,
+  entendiendo bien cómo funciona todo desde el inicio. Como apenas estoy comenzando,
+  quiero fortalecer mis bases, comprender cómo se construyen las interfaces y cómo 
+  lograr que la página sea dinámica e interactiva. También me gustaría ganar más 
+  confianza al programar, organizar mejor mi código y sentir que realmente estoy 
+  entendiendo lo que hago y no solo copiando. En general, espero que este curso me
+  ayude a crecer como desarrolladora y a sentirme más preparada para enfrentar 
+  proyectos reales en el futuro.</p></main>)}
+

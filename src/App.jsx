34a4d8bf4fa42dfb8dc7e1a74b26837ad988a8dd
Mariana Suarez <mariana.suarezh97@gmail.com>
2026-03-02 15:48:31 -0500

    feat: Clase_6_Agenda_ADSO_v4_Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,13 @@ export default function App() {
   };
 
   return (
-    <main className="app-container">
-      <h1 className="app-title">Agenda ADSO v3</h1>
-      <p className="subtitulo">
-        Persistencia con localStorage + UI moderna
-      </p>
+    <main className="max-w-2xl mx-auto mt-10 p-4">
+    <h1 className="text-3xl font-bold text-morado text-center mb-2">
+      Agenda ADSO v4
+    </h1>
+    <p className="text-gray-500 text-center mb-6">
+      Interfaz moderna con TailwindCSS
+    </p>
 
       <FormularioContacto onAgregar={agregarContacto} />
 

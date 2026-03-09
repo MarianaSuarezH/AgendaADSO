61081166b841e2e59a57418d90e7bd9dac465607
Mariana Suarez <mariana.suarezh97@gmail.com>
2026-03-09 15:45:48 -0500

    Clase_9_Agenda_ADSO_v7_Refactor_Documentacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+// importamos React y los hooks necesarios
 import { useEffect, useState } from "react";
 import { listarContactos, crearContacto, eliminarContactoPorId } from "./api";
 import FormularioContacto from "./components/FormularioContacto";
 import ContactoCard from "./components/ContactoCard";
+// Importamos la configuración global de la app
+import { APP_INFO } from "./config";
 
 function App() {
   const [contactos, setContactos] = useState([]);
@@ -60,14 +63,13 @@ function App() {
       <div className="max-w-4xl mx-auto px-4 py-8">
         <header className="mb-8">
           <p className="text-xs tracking-[0.3em] text-gray-500 uppercase">
-            Desarrollo Web ReactJS Ficha 3223876
+            Desarrollo Web ReactJS Ficha {APP_INFO.ficha}
           </p>
           <h1 className="text-4xl font-extrabold text-gray-900 mt-2">
-            Agenda ADSO v6
+            {APP_INFO.titulo}
           </h1>
           <p className="text-sm text-gray-600 mt-1">
-            Gestión de contactos conectada a una API local con JSON Server,
-            ahora con validaciones y mejor experiencia de usuario.
+            {APP_INFO.subtitulo}
           </p>
         </header>
 
